d9c9d74e400847ada7c1e0d34d293093b3adff7e
Programmers3539 <bbprogrammers3539@gmail.com>
2025-01-27 20:09:41 -0500

    Put in Drivetrain ID Constants

--- src/main/java/frc/robot/constants/IDConstants.java
+++ src/main/java/frc/robot/constants/IDConstants.java
@@ -8,16 +8,28 @@ public class IDConstants extends BBConstants {
 		save();
 	}
 
-	public static int rotateMotorID = 1;
-	public static int algaeScoringMotorID = 2;
-	public static int algaeDeployMotorID = 3;
-	public static int algaeIntakeMotorID = 4;
-	public static int coralDeployMotorID = 5;
-	public static int coralIntakeMotorID = 6;
-	public static int clawServoID = 1;
-	public static int elevatorMotorID = 7;
-	public static int pigeonID = 1;
-	public static int climberMotorID = 8;
-	public static int climberCanCoderID = 3;
 	public static int coralDeployCanCoderID = 9;
+	public static int climberMotorID = 8;
+	public static int FLSteeringID = 7;
+	public static int elevatorMotorID = 7;
+	public static int coralIntakeMotorID = 6;
+	public static int FLDriveID = 6;
+	public static int coralDeployMotorID = 5;
+	public static int BLSteeringID = 5;
+	public static int BLDriveID = 4;
+	public static int algaeIntakeMotorID = 4;
+	public static int algaeDeployMotorID = 3;
+	public static int climberCanCoderID = 3;
+	public static int FRSteeringID = 3;
+	public static int algaeScoringMotorID = 2;
+	public static int FRDriveID = 2;
+	public static int BRCanCoderID = 20;
+	public static int clawServoID = 10;
+	public static int pigeonID = 11;
+	public static int rotateMotorID = 12;
+	public static int BRSteeringID = 1;
+	public static int BRDriveID = 0;
+	public static int BLCanCoderID = 21;
+	public static int FRCanCoderID = 22;
+	public static int FLCanCoderID = 23;
 }
